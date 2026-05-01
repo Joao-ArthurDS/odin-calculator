0d3812dea04f208d29efd643dbc7ab1a3a00dbff
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-05-01 19:03:04 -0300

    starting function to operate buttons

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function operate(){
+const numbers = document.querySelectorAll('.numbers');
+const operations = document.querySelectorAll('.operations');
 
+numbers.forEach((number) => addEventListener('click',operate));
+operations.forEach((operation) => addEventListener('click',operate));
+
+function operate(e){
+    let x,y,z;
+    z = e.target.innerHTML;
+    console.log(z);
 }
