11ade43d07c712d7dac04aef36ffe06c13916526
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-05-04 12:08:01 -0300

    working on the operations functions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+let x;
 const numbers = document.querySelectorAll('.numbers');
 const operations = document.querySelectorAll('.operations');
 
-numbers.forEach((number) => addEventListener('click',operate));
-operations.forEach((operation) => addEventListener('click',operate));
+numbers.forEach((number) => number.addEventListener('click',checkNumber));
+operations.forEach((operation) => operation.addEventListener('click',operate));
+
+function checkNumber(e){
+    let n = e.target.innerHTML;
+    let screen = document.querySelector("#visor");
+
+    if (screen.value == 0 && n != 0){
+        screen.value = String(n);
+    } else if (screen.value != 0){
+        screen.value = String(screen.value) + String(n);
+    };
+};
 
 function operate(e){
-    let x,y,z;
-    z = e.target.innerHTML;
+    let y,operation;
+    operation = e.target.id;
     console.log(z);
+
+    if (x === undefined){
+        x = document.querySelector("#visor").value;
+    } else {
+        y = document.querySelector("#visor").value;
+    };
+
+
 }
